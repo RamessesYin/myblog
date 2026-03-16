542878b91e480beed32bc9977c0a1d7d1a858f31
Hao Yin <347712983@qq.com>
2026-03-16 18:53:33 +0800

    Quartz sync: Mar 16, 2026, 6:53 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "RamessesYin.github.io/blog",
+    baseUrl: "RamessesYin.github.io/quartz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
